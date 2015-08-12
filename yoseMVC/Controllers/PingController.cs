69b657626386d9d8327926c03f3c4ed709c3a13d
fadhlulwafi <fadhlulwafi@ymail.com>
2015-08-12 16:05:11 +0800

    init solution
    net

---
+++ yoseMVC/Controllers/PingController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace yoseMVC.Controllers
+{
+    public class PingController : ApiController
+    {
+        // GET api/ping
+        public test Get()
+        {
+            test newTest = new test();
+            newTest.alive = true;
+
+            return newTest;
+        }
+
+
+
+        // GET api/ping/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/ping
+        public test Post([FromBody]string value)
+        {
+            test newTest = new test();
+            newTest.alive = true;
+
+            return newTest;
+        }
+
+        // PUT api/ping/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/ping/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+       public class test
+    {
+        public bool alive;
+    }
+}
+
